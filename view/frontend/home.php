c9c79d04e6d3a88b8b79894db3d7f39526f413f3
Elodie Meunier <elow.m@live.fr>
2019-05-11 15:18:47 +0200

    Création page d'accueil et mise en place du design

---
+++ view/frontend/home.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Success Mission</title>
+    <link rel="stylesheet" href="../public/css/style.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Alegreya+Sans:400,700,900">
+</head>
+<body>
+    <header>
+        <div class="header__bg"></div>
+        <img id="bubble-violet" src="../public/images/big-bubble.svg" alt="bulle violette">
+        <nav>
+            <img id="logo-menu" src="../public/images/logo.svg" alt="Logo Success Mission">
+            <ul>
+                <li>En savoir plus</li>
+                <li>Connexion</li>
+            </ul>
+        </nav>
+        <div class="container-accroche">
+            <div class="accroche">
+                <div class="title">
+                    <h1>Success<br>Mission</h1>
+                    <div class="text">
+                        <div class="barre-verticale"></div>
+                        <p>Manipulez vos tâches avec facilité, organisez votre planning <br> et sélectionnez vos priorités avec souplesse.</p>
+                    </div>
+                    <br>
+                    <br>
+                    <div class="btn">
+                        <a class="btn-look" href="#section">En voir plus</a>
+                        <a href="/" class="btn-inscription">S'inscrire</a>
+                    </div>
+                </div>
+                <img id="logo" src="../public/images/logo.svg" alt="Logo Success Mission">
+            </div>
+        </div>
+        <div class="bubble-blue">
+            <img src="../public/images/bubble_blue.svg" alt="">
+        </div>
+    </header>
+
+    <section id="info">
+        <h2>Accédez aux informations rapidement</h2>
+        <p>
+            Vous souhaitez des détails sur une tâche ? 
+            <br>
+            Ajoutez des images, des dates limites ou encore
+            <br>
+            des commentaires à chacune de vos missions.
+        </p>
+        <aside>
+            <img class="mockup" src="../public/images/mockup.jpg" alt="">
+        </aside>
+    </section>
+
+    
+</body>
+</html>
