4d6a36417b543a726b904031fc2a868e42135dd1
Elodie Meunier <elow.m@live.fr>
2019-05-19 19:25:35 +0200

    Création d'un routeur

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
 <?php
 // Page index
-require "view/frontend/home.php";
+require 'vendor/autoload.php';  
+// require 'view/frontend/home.php';
+$router = new App\Router\Router($_GET['url']);
+
+$router->get('/posts', function(){echo 'Tous les articles';});
+$router->get('/posts:id', function($id){echo 'Affiche l\'article' . $id;});
+$router->post('/posts:id', function($id){echo 'Poste l\'article' . $id;});
+
+$router->run();
