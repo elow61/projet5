c3c87c87b231a2776363adffa0379cff53824edc
Elodie Meunier <elow.m@live.fr>
2019-05-20 18:09:07 +0200

    Ajout possibilité d'appel de controller pour la route

--- class/Router/Route.php
+++ class/Router/Route.php
@@ -28,6 +28,14 @@ class Route {
     }
 
     public function call() {
-        return call_user_func_array($this->callable, $this->matches);
+
+        if (is_string($this->callable)) {
+            $params = explode('#', $this->callable);
+            $controller = "App\\Controller\\" . $params[0];
+            $controller = new $controller();
+            return call_user_func_array([$controller, $params[1], $this->matches]);
+        } else {
+            return call_user_func_array($this->callable, $this->matches);
+        }
     }
 }
--- index.php
+++ index.php
@@ -1,9 +1,10 @@
 <?php
 // Page index
 require 'vendor/autoload.php';  
-// require 'view/frontend/home.php';
 $router = new App\Router\Router($_GET['url']);
 
+$router->get('/', function() {require 'view/frontend/home.php';});
+$router->get('/connexion', function() {require 'view/frontend/connexion.php';});
 $router->get('/posts', function(){echo 'Tous les articles';});
 $router->get('/posts:id', function($id){echo 'Affiche l\'article' . $id;});
 $router->post('/posts:id', function($id){echo 'Poste l\'article' . $id;});
